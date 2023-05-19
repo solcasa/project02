6af829ad2983bfea8e9f1495297b088de04e7776
solcasa <133613660+solcasa@users.noreply.github.com>
2023-05-19 14:05:46 +0900

    2주차 과제
    2주차 과제

---
+++ js/main01.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.mainSlide').slick({
+        // arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        pauseOnHover: false,
+        // 오토플레이 기본 3000, 호버쓰면 마우스 올려놔도 돌아감.
+    });
+
+})
